4114e0d0a0e42483f5fbc16f08c0b88b4f26c2b6
Leandro Ferreira <leandrolima_ferreira@hotmail.com>
2023-09-02 02:48:43 -0300

    feat: Adding repository InMemoryRepository - part 2

--- src/shared/errors/validation-error.ts
+++ src/shared/errors/validation-error.ts
@@ -1,9 +1,7 @@
 import { FieldsErrors } from "../domain/validators/validator-fields-interface";
 
-export class ValidationError extends Error {
-}
 
-export class EntityValidationError extends ValidationError {
+export class EntityValidationError extends Error {
   constructor(public error: FieldsErrors){
     super("Entity validation error")
     this.name = 'EntityValidationError';
