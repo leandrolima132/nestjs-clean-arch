03e02834370012874d909b17b2e3a7a93ada6046
Leandro Ferreira <leandrolima_ferreira@hotmail.com>
2023-09-12 23:03:30 -0300

    feat: Unit tests SeachParams class - part 1

--- src/shared/domain/repositories/searchable-repository-contracts.ts
+++ src/shared/domain/repositories/searchable-repository-contracts.ts
@@ -18,12 +18,12 @@ export class SearchParams {
   protected _sortDir: SortDirection
   protected _filter: string | null;
 
-  constructor(props: SearchProps){
-    this._page = props.page
-    this._perPage = props.perPage
-    this._sort = props.sort
-    this._sortDir = props.sortDir
-    this._filter = props.filter
+  constructor(props: SearchProps = {}){
+    this.page = props.page
+    this.perPage = props.perPage
+    this.sort = props.sort
+    this.sortDir = props.sortDir
+    this.filter = props.filter
   }
 
   get page(): number {
@@ -43,11 +43,11 @@ export class SearchParams {
   }
 
   private set perPage(value: number) {
-    let _perPage = +value
+    let _perPage = value ===(true as any) ? this._perPage : +value
     if(Number.isNaN(_perPage) || _perPage <= 0 || parseInt(_perPage as any) !== _perPage) {
       _perPage = this._perPage
     }
-    this._page = _perPage
+    this._perPage = _perPage
   }
 
   get sort(): string | null {
