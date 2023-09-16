038e2d06bb692d9ff871ccddd69f910eee1ae88a
Leandro Ferreira <leandrolima_ferreira@hotmail.com>
2023-09-16 15:28:25 -0300

    feat: Adding search method in InMemorySearchableRepository class - part 1

--- src/shared/domain/repositories/in-memory.searchable-repository.ts
+++ src/shared/domain/repositories/in-memory.searchable-repository.ts
@@ -1,9 +1,23 @@
 import { Entity } from "../entities/entity";
-import { SearchableRepositoryInterface } from "./searchable-repository-contracts";
+import { SearchParams, SearchResult, SearchableRepositoryInterface } from "./searchable-repository-contracts";
 import { InMemoryRepository } from "./in-memory.repository";
 
 export abstract class InMemorySearchableRepository<E extends Entity> extends InMemoryRepository<E> implements SearchableRepositoryInterface<E, any, any> {
-  search(props: any): Promise<any> {
+  search(props: SearchParams): Promise<SearchResult<E>> {
     throw new Error("Method not implemented.");
   }
+
+  protected abstract applyFilter(items: E[], filter: string | null): Promise<E[]>;
+
+  protected async applySort(
+    items: E[],
+    sort: string | null,
+    sortDir: string | null
+  ): Promise<E[]> {}
+
+  protected async applyPaginate(
+    items: E[],
+    page: SearchParams['page'],
+    perPage: SearchParams['perPage'],
+  ): Promise<E[]> {}
 }
